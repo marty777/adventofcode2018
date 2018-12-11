110f5a91b8a98416170d5e1e139d6c1de508735a
Martin Thorne <marty@michetti.com>
2018-12-10 23:12:46 -0700

    Day11

--- Day11/Day11.go
+++ Day11/Day11.go
@@ -65,7 +65,7 @@ func main() {
 	for i := 0; i < 300 - 3; i++ {
 		for j := 0; j < 300 - 3; j++ {
 			power := grid[i][j] + grid[i+1][j] + grid[i+2][j] + grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1] + grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2] 
-			if(power >= max_power) {
+			if(power > max_power) {
 				max_power = power
 				x = i
 				y = j
