cbdc035cfc6fba030127b3fdd3e6221b973fd966
Marty <marty@michetti.com>
2018-12-19 12:26:07 -0700

    Day19 complete

--- Day19/Day19.go
+++ Day19/Day19.go
@@ -34,7 +34,7 @@ func readLines(path string) ([]string, error) {
 }
 
 type instruction struct {
-	inst [4]uint64
+	inst [4]int
 }
 
 func parseInput(lines[] string)(int, []instruction) {
@@ -42,7 +42,6 @@ func parseInput(lines[] string)(int, []instruction) {
 	fmt.Sscanf(lines[0], "#ip %d", &ip)
 	var program []instruction
 	var instlist = []string{"addr","addi","mulr","muli","banr","bani","borr","bori","setr","seti","gtir","gtri","gtrr","eqir","eqri","eqrr"}
-	fmt.Println(instlist)
 	for i:=1; i<len(lines);i++ {
 	    var inst string
 		var a,b,c int
@@ -54,12 +53,12 @@ func parseInput(lines[] string)(int, []instruction) {
 				break
 			}
 		}
-		program = append(program, instruction{inst: [4]uint64{uint64(inst_i), uint64(a), uint64(b), uint64(c)}})
+		program = append(program, instruction{inst: [4]int{int(inst_i), int(a), int(b), int(c)}})
 	}
 	return ip, program
 }
 
-func execute(inst instruction, state *[6]uint64) {
+func execute(inst instruction, state *[6]int) {
 	switch inst.inst[0] {
 		case 0: // addr
 			state[inst.inst[3]] = state[inst.inst[1]] + state[inst.inst[2]]
@@ -129,35 +128,80 @@ func main() {
 	fmt.Println(len(lines), "lines found in input file")
 	
 	ip, program:=parseInput(lines)
-	var state [6]uint64
+	var state [6]int
 	
-	var index uint64
-	fmt.Println(state)
+	var index int
 	for {
 		state[ip] = index
 		execute(program[index], &state)
 		index = state[ip]
 		index++
-		if(index >= uint64(len(program)) || index < 0) {
+		if(index >= int(len(program)) || index < 0) {
 			break
 		}
 	}
 	fmt.Println("Result A:", state[0]);
 	
-	state = [6]uint64{1,0,0,0,0,0}
-	fmt.Println(state)
+	// Part B - I haven't fully unrolled the program, but I can see that register 0 at program end contains the sum of a set of integers with the product of the value that appears in register 4 after several steps.
+	// The operands in the sum and product seem to be prime. Determine the product by running the program forward until reg 4 is not 0, then perform SOE to decompose the product and obtain the sum = product + (sum of primes in decomposition including 1)
+	state = [6]int{1,0,0,0,0,0}
 	index = 0
+	product := 0
 	for {
-		//fmt.Println(state)
+		reg0:=state[0]
 		state[ip] = index		
 		execute(program[index], &state)
 		index = state[ip]
 		index++
-		if(index >= uint64(len(program)) || index < 0) {
+		
+		if(reg0 != state[0]) {
+			product = state[4]
 			break
 		}
+		if(index >= int(len(program)) || index < 0) {
+			break
+		}
+	
 	}
-	fmt.Println("Result B:", state[0]);
+	
+	product_temp := product
+	
+	// good old sieve of eratosthenes
+	sieve := make([]bool, product+1) // initialized to false
+	for i := 2; i < len(sieve); i++ {
+		sieve[i] = true
+	}
+	// until sqrt(product)
+	for i := 2; i*i <= product; i++ {
+		if sieve[i] == true {
+			for j := i * 2; j <= product; j += i {
+				sieve[j] = false
+			}
+		}
+	}
+	var primes []int
+	for i:=2; i < product; i++ {
+		if(sieve[i]) {
+			primes = append(primes, i)
+		}
+	}
+	
+	var divisors[]int
+	for i:= 0; i < len(primes) && product > 1; i++ {
+		p:=primes[i]
+		temp:=product_temp
+		if (p*(temp/p)) == temp {
+			divisors = append(divisors, p)
+			product_temp = product_temp/p
+		}	
+	}
+	
+	sum := 1 + product
+	for i:= 0; i < len(divisors); i++ {
+		sum += divisors[i]
+	}
+	
+	fmt.Println("Result B:", sum);
 	
 	endtime := getMillis()
 	elapsed := endtime - starttime
